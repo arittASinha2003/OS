10953578fdea36c338b7fdcf7c9cb6a0bad25900
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2023-04-05 01:24:00 +0530

    Add files via upload

---
+++ SJF.c
@@ -0,0 +1,69 @@
+// Program for SJF
+#include<stdio.h>
+int main()
+{
+    int bt[20], p[20], wt[20], tat[20], i, j, n, total = 0, totalT = 0, pos, temp;
+    float avg_wt, avg_tat;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+ 
+    printf("\nEnter Burst Time:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("p%d: ", i+1);
+        scanf("%d", &bt[i]);
+        p[i] = i + 1;  // Creating a process id array
+    }
+ 
+    // Sorting of burst times
+    for(i = 0; i < n; i++)
+    {
+        pos = i;
+        for(j = i + 1; j < n; j++)
+        {
+            if(bt[j] < bt[pos])
+                pos = j;
+        }
+ // Arranging burst times
+        temp = bt[i];
+        bt[i] = bt[pos];
+        bt[pos] = temp;
+ // Arranging process ids
+        temp = p[i];
+        p[i] = p[pos];
+        p[pos] = temp;
+    }
+ 
+    wt[0] = 0;
+ 
+    // Finding the waiting time of all the processes
+    for(i = 1; i < n; i++)
+    {
+        wt[i] = 0;
+        for(j = 0; j < i; j++)
+             // Individual WT by adding BT of all previous completed processes
+            wt[i] += bt[j];
+ 
+        // Total waiting time
+        total += wt[i];   
+    }
+ 
+    // Average waiting time
+    avg_wt = (float)total / n;  
+ 
+    printf("\nProcess\t\t Burst_Time\t\t Waiting_Time\t\t Turnaround_Time");
+    for(i = 0; i < n; i++)
+    {
+        // Turnaround time of individual processes
+        tat[i] = bt[i] + wt[i]; 
+ 
+        // Total turnaround time
+        totalT += tat[i];      
+        printf("\np%d\t\t\t%d\t\t\t%d\t\t\t%d", p[i], bt[i], wt[i], tat[i]);
+    }
+ 
+    // Average turnaround time
+    avg_tat = (float)totalT / n;     
+    printf("\n\nAverage Waiting Time = %f", avg_wt);
+    printf("\nAverage Turnaround Time = %f", avg_tat);
+}
