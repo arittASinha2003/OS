e63619df14704b88577db940f76d362dceec59b9
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2023-04-12 15:42:08 +0530

    Add files via upload

---
+++ FIFO.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+void fifo(int string[20],int n,int size)
+{
+    int frames[n];
+    for (int i=0;i<n;i++)
+        frames[i]=-1;
+    int index=-1;
+    int page_miss=0;
+    int page_hits=0;
+
+    for (int i=0;i<size;i++)
+    {
+        int symbol=string[i];
+        int flag=0;
+
+        for(int j=0;j<n;j++)
+        {
+            if (symbol==frames[j])
+            {
+                flag=1;
+                break;
+            }
+        }
+
+        if (flag==1)
+        {
+            printf("\nSymbol: %d  Frame: ",symbol);
+            for (int j=0;j<n;j++)
+                printf("%d ",frames[j]);
+            page_hits+=1;
+        }
+        else
+        {
+            index=(index+1)%n;
+            frames[index]=symbol;
+            page_miss+=1;
+            printf("\nSymbol: %d  Frame: ",symbol);
+            for (int j=0;j<n;j++)
+                printf("%d ",frames[j]);
+        }
+    }
+    printf("\nPage hits: %d",page_hits);
+    printf("\nPage misses: %d",page_miss);
+}
+
+int main(void)
+{
+    int no_frames, size;
+    printf("Enter no. of page frames: ");
+    scanf("%d", &no_frames);
+    printf("Enter size of the string: ");
+    scanf("%d", &size);
+    int string[size];
+    printf("Enter the values:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &string[i]);
+    }
+    fifo(string, no_frames, size);
+    return 0;
+}
